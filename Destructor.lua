23d26efe1ca364d898c742f45efa195467bf0bb0
Noodle <elliot_grimm_2008@yahoo.com>
2025-10-04 23:19:57 -0700

    Update Destructor.lua

--- Destructor.lua
+++ Destructor.lua
@@ -1,76 +1,50 @@
 --!strict
 --!native
-local Destructor = {} :: DestructorImplementation
-Destructor.__index = Destructor
+local DICTIONARY_DESTRUCTOR_KEYS = {"Destruct", "Destroy"} -- Key(s) to index dictionary for callable destructor.
 
-type DestructorImplementation = {
-	__index: DestructorImplementation,
-	__len: (self: Destructor) -> number,
-	__iter: (self: Destructor) -> (Destructor, number?) -> (number?, any),
+type Integer = number
+type Callback = (...any) -> ...any
+type Dictionary = {[any]: any}
+
+type Varargs<Type> = Type -- Sugar for variable arguments.
+
+type Iterator = (Destructor, Integer?) -> (Integer?, any)
+
+type Destruct = (self: Destructor) -> ()
+
+type Implementation = {
+	__index: Implementation,
+	__len: (self: Destructor) -> Integer,
+	__iter: (self: Destructor) -> Iterator,
 	IsDestructor: (value: any) -> boolean,
 	new: () -> Destructor,
-	Add: <Value>(self: Destructor, value: Value, ...any) -> Value,
+	Add: <Value, Arguments...>(self: Destructor, value: Value, Arguments...) -> Value,
 	Remove: <Value>(self: Destructor, value: Value) -> Value,
-	Destruct: (self: Destructor) -> ()
+	Destruct: Destruct,
+	Destroy: Destruct -- Alias for Destruct method. (*1)
 }
 
-type DestructorProperties = {
+type Properties = {
 	_Values: {any},
-	_Destructing: boolean
+	_Destructing: boolean -- Mutex-like behavior to prevent cyclic hangs. (*2)
 }
 
 export type Destructor = typeof(
 	setmetatable(
-		{} :: DestructorProperties,
-		{} :: DestructorImplementation
+		{} :: Properties,
+		{} :: Implementation
 	)
 )
 
-local Destructors = {
-	["function"] = function(callback: (...any) -> (...any))
-		xpcall(callback, function(message: string)
-			warn(debug.traceback(message))
-		end)
-	end,
-	table = function(source: {[any]: any})
-		xpcall(function()
-			local destruct = source.Destruct
+local Destructor = {} :: Implementation
+Destructor.__index = Destructor
 
-			if type(destruct) == "function" then
-				destruct(source)
-
-				return
-			end
-
-			local destroy = source.Destroy
-
-			if type(destroy) == "function" then
-				destroy(source)
-			end
-		end, function(message: string)
-			warn(debug.traceback(message))
-		end)
-	end,
-	thread = function(thread: thread)
-		pcall(task.cancel, thread)
-	end,
-	RBXScriptConnection = function(connection: RBXScriptConnection)
-		connection:Disconnect()
-	end,
-	Instance = function(instance: Instance)
-		if instance:IsA("Tween") then
-			instance:Cancel()
-		end
-
-		instance:Destroy()
-	end
-}
-
-function Destructor:__len(): number
+function Destructor:__len(): Integer
 	return #self._Values
 end
 
-function Destructor:__iter(): (Destructor, number?) -> (number?, any)
+-- Called for generalized iteration.
+function Destructor:__iter(): Iterator
 	return next, self._Values
 end
 
@@ -85,25 +59,30 @@ function Destructor.new(): Destructor
 	}, Destructor)
 end
 
-function Destructor:Add<Value>(value: Value, ...: any): Value
+function Destructor:Add<Value, Arguments...>(value: Value, ...: Arguments...): Value
 	local entry: any = value
 
 	if type(value) == "function" then
-		assert(not self._Destructing, `Called {self.Add} on {self} with argument 'Value' as {value} and not function or while property '_Destructing' is {self._Destructing} and not falsy.`)
+		-- *2
+		assert(not self._Destructing, `Called method 'Add' on {self} with argument 'Value' as {value} and not a function or while property '_Destructing' is {self._Destructing} and not falsy.`)
 
-		if select("#", ...) ~= 0 then
-			local varargs = {...}
+		-- table.pack return comprises key 'n' indicating arity; ignored by unpack.
+		local varargs = table.pack(...)
 
-			local function _DestructorThunk()
+		-- Only wrap if varargs are provided to minimize compute time & memory pressure.
+		if varargs.n ~= 0 then
+			-- Define on separate line than entry assignment to preserve function name for traceback.
+			local function _DestructorThunkWrapper()
 				value(unpack(varargs))
 			end
 
-			entry = _DestructorThunk
+			entry = _DestructorThunkWrapper
 		end
 	end
 
 	table.insert(self._Values, entry)
 
+	-- Always return value argument, never wrapper if assigned to entry.
 	return value
 end
 
@@ -111,11 +90,53 @@ function Destructor:Remove<Value>(value: Value): Value
 	local values = self._Values
 	local index = table.find(values, value)
 
+	-- Return entry if found.
 	return index and table.remove(values, index) :: any
 end
 
+-- Error handler for destructors calling provided callbacks.
+local function OnError(message: string)
+	warn(debug.traceback(message))
+end
+
+local Destructors = {
+	Instance = function(instance: Instance)
+		-- Pause if Tween; Destroy does not halt playback.
+		if instance:IsA("Tween") then
+			instance:Pause()
+		end
+
+		instance:Destroy()
+	end,
+	RBXScriptConnection = function(connection: RBXScriptConnection)
+		connection:Disconnect()
+	end,
+	["function"] = function(callback: Callback)
+		xpcall(callback, OnError)
+	end,
+	table = function(source: Dictionary)
+		xpcall(function()
+			-- Call first found destructor.
+			for _, key in DICTIONARY_DESTRUCTOR_KEYS do
+				local value = source[key]
+
+				if type(value) == "function" then
+					value(source)
+
+					return
+				end
+			end
+		end, OnError)
+	end,
+	thread = function(thread: thread)
+		-- Call in protected mode; throws error if thread is running [[nested] coroutine(s)].
+		pcall(task.cancel, thread)
+	end
+}
+
 function Destructor:Destruct()
-	assert(not self._Destructing, `Called {self.Destruct} on {self} while property '_Destructing' is {self._Destructing} and not falsy.`)
+	-- *2
+	assert(not self._Destructing, `Called method 'Destruct' on {self} while property '_Destructing' is {self._Destructing} and not falsy.`)
 
 	self._Destructing = true
 
@@ -136,5 +157,8 @@ function Destructor:Destruct()
 
 	self._Destructing = false
 end
+
+-- *1
+Destructor.Destroy = Destructor.Destruct
 
 return Destructor
