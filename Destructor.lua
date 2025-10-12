971c18ae3f046652f4cd1d5a9e5ba8c598e5da44
Noodle <elliot_grimm_2008@yahoo.com>
2025-10-11 20:41:54 -0700

    Update Destructor.lua

--- Destructor.lua
+++ Destructor.lua
@@ -1,7 +1,7 @@
 --!strict
 --!native
 local DICTIONARY_DESTRUCTOR_KEYS = {"Destruct", "Destroy"} -- Key(s) to index dictionary for successful destructor.
-local PERSISTER_MAX_DEPTH = 3 -- Consecutive Persister call limit to prevent cyclic re-entry hangs. (*1)
+local THROW_WARNING_NO_DESTRUCTOR = true -- Throw warning if no destructor for data type.
 
 type UInt = number -- [0 .. ∞)
 type Array = {any}
@@ -16,18 +16,18 @@ type Implementation = {
 	__len: (self: Destructor) -> UInt,
 	__iter: (self: Destructor) -> (Iterator, Array),
 	IsDestructor: (value: any) -> boolean, -- Returns a *boolean* indicating whether `value` is a *Destructor*.
-	new: (_values: Array?) -> Destructor, -- Returns a new *Destructor* object.
+	new: () -> Destructor, -- Returns a new *Destructor* object.
 	Extend: (self: Destructor, once: boolean?) -> Destructor, -- Returns a new sub-*Destructor* object that calls `Destruct` when the parent *Destructor* `self` calls `Destruct`. If `once` is *true*, `Destruct` will only be called once.
 	Add: <Value>(self: Destructor, value: Value, ...VarArgs<any>) -> Value, -- Adds `value` to the *Destructor*. If `value` is a *function*, it will be thunked with varargs `...`, and will throw an error if `Destruct` is executing.
 	Remove: <Value>(self: Destructor, value: Value, all: boolean?) -> VarArgs<Value>, -- Removes the first value matching `value` from the *Destructor* and returns it if found. If `all` is *true*, all values matching `value` will be removed and returned, not just the first.
 	Clear: (self: Destructor) -> (), -- Removes all values from the *Destructor* without destructing them.
-	Destruct: Destruct, -- Destructs and removes all values from the *Destructor*. Throws an error if called during execution. *3
-	Destroy: Destruct -- Alias for the `Destruct` method. (*2)
+	Destruct: Destruct, -- Destructs and removes all values from the *Destructor*. Throws an error if called during execution. *2
+	Destroy: Destruct -- Alias for the `Destruct` method. (*1)
 }
 
 type Properties = {
 	_Values: Array,
-	_IsDestructing: boolean -- Mutex-like behavior to prevent cyclic re-entry hangs. *1 -> (*3)
+	_IsDestructing: boolean -- Mutex-like behavior to prevent cyclic re-entry hangs. (*2)
 }
 
 export type Destructor = typeof(
@@ -52,50 +52,51 @@ function Destructor.IsDestructor(value: any): boolean
 	return type(value) == "table" and getmetatable(value) == Destructor
 end
 
-function Destructor.new(_values: Array?): Destructor
+function Destructor.new(): Destructor
 	return setmetatable({
-		_Values = _values or {},
+		_Values = {},
 		_IsDestructing = false
 	}, Destructor)
+end
+
+-- Error handler for destructors calling provided callbacks.
+local function _DestructorErrorHandler(message: string)
+	warn(debug.traceback(message))
 end
 
 function Destructor:Extend(once: boolean?): Destructor
 	assert(not once or once == true, `Argument 'Once' to method 'Extend' on {self} is {once} and not a boolean or nil.`)
 
-	local destructor = Destructor.new({self :: any, nil})
+	local destructor = self:Add(Destructor.new())
 
 	if once then
-		self:Add(destructor)
-
 		return destructor
 	end
 
-	local Persister: Persister
+	-- Pack table.insert arguments for brevity.
+	local Persister: Persister, ArgumentPackages: {Array}
 
-	-- Define on separate line from assignment to preserve function name for traceback. (*4)
-	local function _DestructorEntryPersister(depth: UInt?)
-		depth = depth or 1
+	-- Define on separate line from assignment to preserve function name for traceback. (*3)
+	local function _DestructorEntryPersister()
+		task.defer(function()
+			for _, argumentPackage in ArgumentPackages do
+				local values: Array, value = unpack(argumentPackage)
 
-		local values = self._Values
-
-		task.defer(xpcall, function()
-			table.insert(values, 1, destructor)
-			table.insert(values, 2, Persister)
-		end, function(message: string)
-			warn(debug.traceback(message))
-
-			-- *1
-			if depth ~= PERSISTER_MAX_DEPTH then
-				Persister(depth :: any + 1)
-
-				return
+				-- Only insert if not at index.
+				if values[1] ~= value then
+					table.insert(values, 1, value)
+				end
 			end
-
-			warn(`Variable 'Depth' is {depth} and equal to constant 'PERSISTER_MAX_DEPTH' as {PERSISTER_MAX_DEPTH}.`)
 		end)
 	end
 
 	Persister = _DestructorEntryPersister
+
+	ArgumentPackages = {
+		{self._Values, destructor},
+		{destructor._Values, Persister}
+	}
+
 	Persister()
 
 	type Persister = typeof(Persister)
@@ -107,7 +108,7 @@ function Destructor:Add<Value>(value: Value, ...: VarArgs<any>): Value
 	local entry: any = value
 
 	if type(value) == "function" then
-		-- *3
+		-- *2
 		assert(not self._IsDestructing, `Called method 'Add' on {self} with argument 'Value' as {value} and not a function or while variable 'IsDestructing' is {self._IsDestructing} and not falsy.`)
 
 		-- table.pack return comprises key 'n' indicating arity; ignored by unpack.
@@ -115,7 +116,7 @@ function Destructor:Add<Value>(value: Value, ...: VarArgs<any>): Value
 
 		-- Only wrap if varargs are provided to minimize compute time & memory pressure.
 		if varargs.n ~= 0 then
-			-- *4
+			-- *3
 			local function _DestructorThunkWrapper()
 				value(unpack(varargs))
 			end
@@ -173,7 +174,7 @@ type Destructors = (any) -> ()
 local Destructors: {[TypeNames]: Destructors}
 
 do
-	-- *4
+	-- *3
 	local function _InstanceDestructor(instance: Instance)
 		-- Pause if Tween; Destroy does not halt playback.
 		if instance:IsA("Tween") then
@@ -185,11 +186,6 @@ do
 
 	local function _ConnectionDestructor(connection: RBXScriptConnection)
 		connection:Disconnect()
-	end
-
-	-- Error handler for destructors calling provided callbacks.
-	local function _DestructorErrorHandler(message: string)
-		warn(debug.traceback(message))
 	end
 
 	type Callback = (...any) -> ...any
@@ -241,30 +237,36 @@ do
 end
 
 function Destructor:Destruct()
-	-- *3
+	-- *2
 	assert(not self._IsDestructing, `Called method 'Destruct' on {self} while variable 'IsDestructing' is {self._IsDestructing} and not falsy.`)
 
 	self._IsDestructing = true
 
 	local values = self._Values
-	local index, value = next(values)
 
-	while value ~= nil do
-		values[index :: any] = nil
+	while true do
+		local index, value = next(values)
 
-		local destructor = Destructors[typeof(value)]
+		if index == nil then
+			break
+		end
+
+		values[index] = nil
+
+		local key = typeof(value)
+		local destructor = Destructors[key]
 
 		if destructor then
 			destructor(value)
+		elseif THROW_WARNING_NO_DESTRUCTOR then
+			warn(`Called method 'Destruct' on {self} while type of argument '{value}' is {key} and index of {Destructors} is {destructor} and not a function.`)
 		end
-
-		index, value = next(values)
 	end
 
 	self._IsDestructing = false
 end
 
--- *2
+-- *1
 Destructor.Destroy = Destructor.Destruct
 
 return Destructor
